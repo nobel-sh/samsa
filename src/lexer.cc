a85100946cc2a586ab2aaa316e6e028e201e4184
nobel <nobel2073@gmail.com>
2024-03-25 18:40:09 +0545

    Add a basic lexer

---
+++ src/lexer.cc
@@ -0,0 +1,116 @@
+#include "lexer.h"
+
+void
+Lexer::lex() {
+    while (!is_eof()) {
+       // std::cout << "current pos: " << pos << std::endl;
+      auto curr_char = peek_token();
+       // std::cout << "current char: " << curr_char << std::endl;
+      switch (curr_char){
+       case '(':
+         tokens.push_back(
+           Token(pos, TokenKind::LPAREN, "("));
+         break;
+      case ')':
+         tokens.push_back(
+           Token(pos, TokenKind::RPAREN, ")"));
+         break;
+      case ' ':
+        while (peek_token()==' ')
+         consume_token();
+        continue;
+      case '{':
+         tokens.push_back(
+           Token(pos, TokenKind::LBRACE, "{"));
+         break;
+      case '}':
+         tokens.push_back(
+           Token(pos, TokenKind::RBRACE, "}"));
+         break;
+       case ',':
+         tokens.push_back(
+           Token(pos, TokenKind::COMMA, ","));
+         break;
+      case '!':
+         tokens.push_back(
+           Token(pos, TokenKind::BANG , "!"));
+         break;
+      case '=':
+         tokens.push_back(
+           Token(pos, TokenKind::EQUALS, "="));
+         break;
+      case '"':
+        handle_raw_instruction();
+        continue;
+      default:
+        if (isalpha(curr_char)){
+          handle_ident();
+          continue;
+        }else{
+          std::cerr << "Unknown token found at: " << pos << std::endl;
+        }
+        break;
+      }
+      consume_token();
+    }
+    tokens.push_back(
+      Token(pos, TokenKind::ENDOFFILE, "EOF"));
+}
+
+void
+Lexer::handle_raw_instruction(){
+      consume_token();
+      char curr_char = peek_token();
+      auto start_pos = pos;
+      while (curr_char !='"'){
+        curr_char = peek_token();
+        if (is_eof())
+          std::cerr << "Non terminated String at: "<< pos << std::endl;
+        consume_token();
+      }
+      std::string instruction(source.begin() + start_pos, source.begin() + pos - 1);
+      tokens.push_back(
+        Token(start_pos,TokenKind::INSTR, std::move(instruction)));
+}
+
+void
+Lexer::handle_ident(){
+      auto start_pos = pos;
+      while (isalnum(peek_token())){
+        consume_token();
+      }
+      std::string str(source.begin() + start_pos, source.begin() + pos);
+      tokens.push_back(
+        Token(start_pos,TokenKind::IDENT, std::move(str)));
+}
+
+void
+Lexer::dump() {
+    for (auto tok : tokens) {
+      tok.dump();
+    }
+}
+
+unsigned char
+Lexer::peek_token() { return peek_token(0); }
+
+unsigned char
+Lexer::peek_token(int nth) {
+    if (pos + nth >= source.size())
+      return EOF;
+    return source[pos + nth];
+}
+
+void
+Lexer::consume_token() { consume_token(1); }
+
+void
+Lexer::consume_token(int n) {
+    if (pos + n >= source.size())
+      pos = source.size();
+    else
+      pos += n;
+}
+
+bool
+Lexer::is_eof() { return pos >= source.size(); }
---
+++ src/token.h
@@ -0,0 +1,50 @@
+#ifndef TOKEN_H_
+#define TOKEN_H_
+
+#include <iostream>
+#include <memory>
+#include <map>
+
+enum TokenKind {
+  IDENT,
+  BANG,
+  LPAREN,
+  RPAREN,
+  EQUALS,
+  LBRACE,
+  RBRACE,
+  INSTR,
+  COMMA,
+  ENDOFFILE,
+};
+
+
+class Token {
+public:
+    Token(unsigned int pos, TokenKind kind, std::string &&str)
+      : pos(pos), kind(kind), str(str) {}
+
+    void dump();
+    std::string to_string(TokenKind kind);
+
+		TokenKind kind;
+		std::string str;
+private:
+		unsigned int pos;
+
+		std::map<TokenKind, std::string> token_string = {
+      {TokenKind::IDENT,  "IDENT"},
+      {TokenKind::BANG,   "BANG"},
+      {TokenKind::LPAREN, "LPAREN",},
+      {TokenKind::RPAREN, "RPAREN",},
+      {TokenKind::EQUALS, "EQUALS",},
+      {TokenKind::LBRACE, "LBRACE,"},
+      {TokenKind::RBRACE, "RBRACE,"},
+      {TokenKind::INSTR,  "INSTR,"},
+      {TokenKind::COMMA,  "COMMA"},
+      {TokenKind::ENDOFFILE, "ENDOFFILE"},
+
+    };
+};
+
+#endif // TOKEN_H_
